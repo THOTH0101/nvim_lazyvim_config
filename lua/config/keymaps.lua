f205cf8fe48a65b1b5f77c6baef375b2281324fd
thoth <omolaraadebayowahab@gmail.com>
2026-02-19 14:12:09 +0100

    change to a different multicursor plugin with different remap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,28 +11,4 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- vim-visual-multi-cursor keymaps config
-
--- delay function
-local function visual_cursors_with_delay()
-  -- Execute the vm-visual-cursors command.
-  vim.cmd('silent! execute "normal! \\<Plug>(VM-Visual-Cursors)"')
-  -- Introduce delay via VimScript's 'sleep' (set to 500 milliseconds here).
-  vim.cmd("sleep 200m")
-  -- Press 'A' in normal mode after the delay.
-  vim.cmd('silent! execute "normal! A"')
-end
-
-local wk = require("which-key")
-
-wk.add({
-  { "<leader>m", group = "Visual Multi" },
-
-  { "<leader>ma", "<Plug>(VM-Select-All)<Tab>", desc = "Select All", mode = "n" },
-  { "<leader>mr", "<Plug>(VM-Start-Regex-Search)", desc = "Start Regex Search", mode = "n" },
-  { "<leader>mp", "<Plug>(VM-Add-Cursor-At-Pos)", desc = "Add Cursor At Pos", mode = "n" },
-  { "<leader>mv", visual_cursors_with_delay, desc = "Visual Cursors", mode = "v" },
-  { "<leader>mo", "<Plug>(VM-Toggle-Mappings)", desc = "Toggle Mapping", mode = "n" },
-})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste Without Clipboard Change" })
